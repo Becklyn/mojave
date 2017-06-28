6491fbbb65b2821a1b04fc6d78fdd7dc9f7a5843
Jannik Zschiesche <jz@becklyn.com>
2017-06-28 15:25:35 +0200

    Simplify test HTML

--- tests/cases/dom/traverse/find.js
+++ tests/cases/dom/traverse/find.js
@@ -3,24 +3,13 @@ QUnit.module(
     {
         beforeEach: function ()
         {
-            const child1 = document.createElement("div");
-            child1.classList.add("test-element-find", "element1");
-
-            const child2 = document.createElement("div");
-            child2.classList.add("test-element-find", "element2");
-
-            const child2_1 = document.createElement("div");
-            child2_1.classList.add("test-element-find", "element2-1");
-            child2.appendChild(child2_1);
-
-            const child3 = document.createElement("div");
-            child3.classList.add("test-element-find", "element3");
-
-            const fixture = document.getElementById("qunit-fixture");
-            fixture.appendChild(child1);
-            fixture.appendChild(child2);
-            fixture.appendChild(document.createTextNode("Test123"));
-            fixture.appendChild(child3);
+            document.getElementById("qunit-fixture").innerHTML =
+                '<div class="test-element-find element1"></div>' +
+                '<div class="test-element-find element2">'+
+                    '<div class="test-element-find element2-1"></div>' +
+                '</div>' +
+                'Some text' +
+                '<div class="test-element-find element3"></div>';
         }
     }
 );
@@ -53,5 +42,34 @@ QUnit.test(
         assert.ok(result[1].classList.contains("element2"), "contains .element2");
         assert.ok(result[2].classList.contains("element2-1"), "contains .element2-1");
         assert.ok(result[3].classList.contains("element3"), "contains .element3");
+    }
+);
+
+
+QUnit.test(
+    "global find() + find order",
+    function (assert)
+    {
+        const find = mojave.dom.traverse.find;
+
+        const result = find(".test-element-find");
+
+        assert.equal(result.length, 4, "found 4 elements");
+        assert.ok(result[0].classList.contains("element1"), "contains .element1");
+        assert.ok(result[1].classList.contains("element2"), "contains .element2");
+        assert.ok(result[2].classList.contains("element2-1"), "contains .element2-1");
+        assert.ok(result[3].classList.contains("element3"), "contains .element3");
+    }
+);
+
+
+QUnit.test(
+    "findOne() matches first result",
+    function (assert)
+    {
+        const findOne = mojave.dom.traverse.findOne;
+
+        const result = findOne(".test-element-find");
+        assert.ok(result.classList.contains("element1"), "contains .element1");
     }
 );

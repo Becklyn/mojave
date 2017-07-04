6206bea081aa738d00915447bd3a118cc30a1311
Patrick Kontschak <pk@becklyn.com>
2017-07-04 10:50:24 +0200

    use find function and add copy changes
    Signed-off-by: Patrick Kontschak <pk@becklyn.com>

--- tests/cases/dom/traverse/filter.js
+++ tests/cases/dom/traverse/filter.js
@@ -4,13 +4,13 @@ QUnit.module(
         beforeEach: function ()
         {
             document.getElementById("qunit-fixture").innerHTML =
-                '<div class="test-element-filter element1"></div>' +
-                '<div class="test-element-filter element2">'+
-                    '<div class="test-element-filter element2-1"></div>' +
+                '<div class="test-element element1"></div>' +
+                '<div class="test-element element2">'+
+                    '<div class="test-element element2-1"></div>' +
                 '</div>' +
                 'Some text' +
-                '<div class="test-element-filter element3"></div>' +
-                '<div class="test-element-filter element4"></div>';
+                '<div class="test-element element3"></div>' +
+                '<div class="test-element element4"></div>';
         }
     }
 );
@@ -20,12 +20,13 @@ QUnit.test(
     "filter() on element with specific class",
     function (assert)
     {
+        const find = mojave.dom.traverse.find;
         const filter = mojave.dom.traverse.filter;
-        const elements = Array.from(document.querySelectorAll(".test-element-filter"));
+        const elements = find(".test-element");
 
         const result = filter(elements, ".element2");
 
         assert.equal(result.length, 1, "found 1 element");
-        assert.ok(result[0].classList.contains("element2"), "contains .element2");
+        assert.ok(result[0].classList.contains("element2"), "found .element2");
     }
 );
--- tests/cases/dom/traverse/not.js
+++ tests/cases/dom/traverse/not.js
@@ -4,15 +4,15 @@ QUnit.module(
         beforeEach: function ()
         {
             document.getElementById("qunit-fixture").innerHTML =
-                '<div class="test-element-not element1"></div>' +
-                '<div class="test-element-not element2">'+
-                    '<div class="test-element-not element2-1"></div>' +
+                '<div class="test-element element1"></div>' +
+                '<div class="test-element element2">'+
+                    '<div class="test-element element2-1"></div>' +
                 '</div>' +
                 'Some text' +
-                '<div class="test-element-not element3">' +
-                    '<div class="test-element-not element3-1"></div>' +
+                '<div class="test-element element3">' +
+                    '<div class="test-element element3-1"></div>' +
                 '</div>' +
-                '<div class="test-element-not element4"></div>';
+                '<div class="test-element element4"></div>';
         }
     }
 );
@@ -22,17 +22,18 @@ QUnit.test(
     "not() on element with specific class",
     function (assert)
     {
+        const find = mojave.dom.traverse.find;
         const not = mojave.dom.traverse.not;
-        const elements = Array.from(document.querySelectorAll(".test-element-not"));
+        const elements = find(".test-element");
 
         const result = not(elements, ".element2");
 
         assert.equal(result.length, 5, "found 5 elements");
-        assert.ok(result[0].classList.contains("element1"), "contains .element1");
-        assert.ok(result[1].classList.contains("element2-1"), "contains .element2-1");
-        assert.ok(result[2].classList.contains("element3"), "contains .element3");
-        assert.ok(result[3].classList.contains("element3-1"), "contains .element3-1");
-        assert.ok(result[4].classList.contains("element4"), "contains .element4");
+        assert.ok(result[0].classList.contains("element1"), "found .element1");
+        assert.ok(result[1].classList.contains("element2-1"), "found .element2-1");
+        assert.ok(result[2].classList.contains("element3"), "found .element3");
+        assert.ok(result[3].classList.contains("element3-1"), "found .element3-1");
+        assert.ok(result[4].classList.contains("element4"), "found .element4");
     }
 );
 
@@ -41,16 +42,17 @@ QUnit.test(
     "not() on elements that are children",
     function (assert)
     {
+        const find = mojave.dom.traverse.find;
         const not = mojave.dom.traverse.not;
-        const elements = Array.from(document.querySelectorAll(".test-element-not"));
+        const elements = find(".test-element");
         const selector = ".test-element-not > :first-child";
 
         const result = not(elements, selector);
 
         assert.equal(result.length, 4, "found 4 elements");
-        assert.ok(result[0].classList.contains("element1"), "contains .element1");
-        assert.ok(result[1].classList.contains("element2"), "contains .element2");
-        assert.ok(result[2].classList.contains("element3"), "contains .element3");
-        assert.ok(result[3].classList.contains("element4"), "contains .element4");
+        assert.ok(result[0].classList.contains("element1"), "found .element1");
+        assert.ok(result[1].classList.contains("element2"), "found .element2");
+        assert.ok(result[2].classList.contains("element3"), "found .element3");
+        assert.ok(result[3].classList.contains("element4"), "found .element4");
     }
 );

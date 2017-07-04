00b2e36ac1d0ce9b440409eef9f0665ca7613f9e
Patrick Kontschak <pk@becklyn.com>
2017-07-04 10:51:11 +0200

    add 2 additional tests
    test for multiple elements with the same class and for a selector that doesn’t exist
    
    Signed-off-by: Patrick Kontschak <pk@becklyn.com>

--- tests/cases/dom/traverse/filter.js
+++ tests/cases/dom/traverse/filter.js
@@ -5,12 +5,12 @@ QUnit.module(
         {
             document.getElementById("qunit-fixture").innerHTML =
                 '<div class="test-element element1"></div>' +
-                '<div class="test-element element2">'+
+                '<div class="test-element test-class element2">'+
                     '<div class="test-element element2-1"></div>' +
                 '</div>' +
                 'Some text' +
                 '<div class="test-element element3"></div>' +
-                '<div class="test-element element4"></div>';
+                '<div class="test-element test-class element4"></div>';
         }
     }
 );
@@ -28,5 +28,37 @@ QUnit.test(
 
         assert.equal(result.length, 1, "found 1 element");
         assert.ok(result[0].classList.contains("element2"), "found .element2");
+    }
+);
+
+
+QUnit.test(
+    "filter() on multiple elements with specific class",
+    function (assert)
+    {
+        const find = mojave.dom.traverse.find;
+        const filter = mojave.dom.traverse.filter;
+        const elements = find(".test-element");
+
+        const result = filter(elements, ".test-class");
+
+        assert.equal(result.length, 2, "found 2 elements");
+        assert.ok(result[0].classList.contains("element2"), "found .element2");
+        assert.ok(result[1].classList.contains("element4"), "found .element4");
+    }
+);
+
+
+QUnit.test(
+    "filter() on element with a wrong class",
+    function (assert)
+    {
+        const find = mojave.dom.traverse.find;
+        const filter = mojave.dom.traverse.filter;
+        const elements = find(".test-element");
+
+        const result = filter(elements, ".no-element");
+
+        assert.equal(result.length, 0, "found 0 elements");
     }
 );

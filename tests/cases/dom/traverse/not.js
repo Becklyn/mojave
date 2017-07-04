a6870a4894853d8bad1e99b2420c1661f6009bf6
Patrick Kontschak <pk@becklyn.com>
2017-07-04 15:31:20 +0200

    add unit test without selector
    Signed-off-by: Patrick Kontschak <pk@becklyn.com>

--- tests/cases/dom/traverse/not.js
+++ tests/cases/dom/traverse/not.js
@@ -56,3 +56,24 @@ QUnit.test(
         assert.ok(result[3].classList.contains("element4"), "found .element4");
     }
 );
+
+
+QUnit.test(
+    "not() without selector",
+    function (assert)
+    {
+        const find = mojave.dom.traverse.find;
+        const not = mojave.dom.traverse.not;
+        const elements = find(".test-element");
+
+        const result = not(elements);
+
+        assert.equal(result.length, 6, "found 6 elements");
+        assert.ok(result[0].classList.contains("element1"), "found .element1");
+        assert.ok(result[1].classList.contains("element2"), "found .element2");
+        assert.ok(result[2].classList.contains("element2-1"), "found .element2-1");
+        assert.ok(result[3].classList.contains("element3"), "found .element3");
+        assert.ok(result[4].classList.contains("element3-1"), "found .element3-1");
+        assert.ok(result[5].classList.contains("element4"), "found .element4");
+    }
+);

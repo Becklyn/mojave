a2ee6e7574ebefecdbe7979f0890240bfddb9a30
Jannik Zschiesche <jz@becklyn.com>
2017-07-05 11:05:17 +0200

    Implement dom/classes with addClass and removeClass

---
+++ dom/classes.js
@@ -0,0 +1,40 @@
+/**
+ * Adds all given classes to the element
+ *
+ * @param {HTMLElement|HTMLElement[]} element
+ * @param {string|string[]} classes
+ */
+export function addClass (element, classes)
+{
+    const list = Array.isArray(element) ? element : [element];
+    const classList = Array.isArray(classes) ? classes : [classes];
+
+    for (let i = 0; i < list.length; i++)
+    {
+        for (let j = 0; j < classList.length; j++)
+        {
+            list[i].classList.add(classList[j]);
+        }
+    }
+}
+
+
+/**
+ * Remove all given classes from the element
+ *
+ * @param {HTMLElement|HTMLElement[]} element
+ * @param {string|string[]} classes
+ */
+export function removeClass (element, classes)
+{
+    const list = Array.isArray(element) ? element : [element];
+    const classList = Array.isArray(classes) ? classes : [classes];
+
+    for (let i = 0; i < list.length; i++)
+    {
+        for (let j = 0; j < classList.length; j++)
+        {
+            list[i].classList.remove(classList[j]);
+        }
+    }
+}

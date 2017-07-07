69aeb8119ea12ce06e6d7bd0f0e6c8dd5064f650
Jannik Zschiesche <jz@becklyn.com>
2017-07-07 17:44:48 +0200

    Make debounce() tests more robust (and don’t collide with the global timeout)

--- tests/cases/timing/debounce.js
+++ tests/cases/timing/debounce.js
@@ -1,3 +1,7 @@
+QUnit.module("timing/debounce");
+
+import {debounce} from "../../../timing/timing";
+
 /**
  * NOTE:
  *
@@ -8,9 +12,20 @@ QUnit.test(
     "only call debounced function once if called in short order",
     function (assert)
     {
-        var done = assert.async();
-        var called = 0;
-        var debounced = mojave.timing.debounce(function () { called += 1; }, 100);
+        const done = assert.async();
+        let called = 0;
+        const debounced = debounce(
+        () => {
+            assert.ok(true, "debounce called");
+            called += 1;
+
+            if (called === 3)
+            {
+                done();
+            }
+        },
+        100
+    );
 
         debounced();
         debounced();
@@ -19,7 +34,7 @@ QUnit.test(
         window.setTimeout(function () {
             assert.equal(called, 1, "the debounced function should only been called once");
             done();
-        }, 1000);
+        }, 600);
     }
 );
 
@@ -28,18 +43,26 @@ QUnit.test(
     "debounced functions can be called multiple times if the time between the calls is long enough",
     function (assert)
     {
-        var done = assert.async();
-        var called = 0;
-        var debounced = mojave.timing.debounce(function () { called += 1; }, 50);
+        assert.expect(3);
+        const done = assert.async();
+        let called = 0;
+
+        const debounced = debounce(
+            () => {
+                assert.ok(true, "debounce called");
+                called += 1;
+
+                if (called === 3)
+                {
+                    done();
+                }
+            },
+            50
+        );
 
         debounced();
         window.setTimeout(debounced, 200);
         window.setTimeout(debounced, 400);
-
-        window.setTimeout( function () {
-            assert.equal(called, 3, "the debounced function should have been called three times");
-            done();
-        }, 1000);
     }
 );
 
@@ -48,17 +71,23 @@ QUnit.test(
     "the debounce delay can be changed",
     function (assert)
     {
-        var done = assert.async();
-        var called = 0;
-        var debounced = mojave.timing.debounce(function () { called += 1; }, 300);
+        const done = assert.async();
+        let called = 0;
+        const debounced = debounce(
+            () => {
+                assert.ok(true, "debounce called");
+                called += 1;
+
+                if (called === 2)
+                {
+                    done();
+                }
+            },
+            300
+        );
 
         debounced();
         window.setTimeout(debounced, 100);
         window.setTimeout(debounced, 500);
-
-        window.setTimeout(function () {
-            assert.equal(called, 2, "the debounced function should have been called two times");
-            done();
-        }, 1000);
     }
 );

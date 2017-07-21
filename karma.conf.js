1c080ae9b7d79ba8e2573c7fe245c0eba617f526
Jannik Zschiesche <jz@becklyn.com>
2017-07-21 19:04:45 +0200

    CS in the karma runner config

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // Generated on Fri Jul 07 2017 16:49:19 GMT+0200 (CEST)
 
-const customLaunchers = {
+const browsers = {
     // Chrome (Windows)
     win_chrome_latest: {
         base: 'SauceLabs',
@@ -121,11 +121,9 @@ module.exports = function (config) {
         // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
 
-
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['qunit'],
-
 
         // list of files / patterns to load in the browser
         files: [
@@ -133,15 +131,12 @@ module.exports = function (config) {
             'tests/dist/all-tests.js'
         ],
 
-
         // list of files to exclude
         exclude: [],
-
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {},
-
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
@@ -149,29 +144,23 @@ module.exports = function (config) {
         //reporters: ['progress'],
         reporters: ['dots', 'saucelabs'],
 
-
         // web server port
         port: 9876,
 
-
         // enable / disable colors in the output (reporters and logs)
         colors: true,
-
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
 
-
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
-
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         // browsers: ['Chrome'],
-        browsers: Object.keys(customLaunchers),
-
+        browsers: Object.keys(browsers),
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
@@ -185,13 +174,18 @@ module.exports = function (config) {
         // use high timeout to better handle long running iOS tasks
         captureTimeout: 180000,
 
+        // SauceLabs specific config
         sauceLabs: {
             testName: 'mojave',
             connectOptions: {
                 verbose: true,
                 verboseDebugging: true,
             },
+
+            recordScreenshots: true,
         },
-        customLaunchers: customLaunchers,
+
+        // All browsers
+        customLaunchers: browsers,
     });
 };

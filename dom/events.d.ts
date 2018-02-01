05d57b587b5b4c137694d9d0f4d299508d99035b
Jannik Zschiesche <jz@becklyn.com>
2018-02-01 17:36:14 +0100

    Add typescript bindings for dom/events

---
+++ dom/events.d.ts
@@ -0,0 +1,62 @@
+type EventRegistrationToken = Function;
+
+
+/**
+ * Registers an event listener for the given events
+ */
+declare function on (
+    element : HTMLElement | Array<HTMLElement|Window>,
+    type : string,
+    handler : (Event) => void,
+) : void;
+
+
+/**
+ * Removes an event listener for the given events
+ */
+declare function off (
+    element : HTMLElement | Array<HTMLElement|Window>,
+    type : string,
+    handler : (Event) => void | EventRegistrationToken,
+) : void;
+
+
+/**
+ * Registers an event listener, that is automatically removed after it was executed once.
+ *
+ * Returns the intermediate function, so that the event listener can be removed:
+ *
+ *      const intermediate = once(element, event, handler);
+ *      off(element, event, intermediate);
+ */
+declare function once (
+    element : HTMLElement | Window,
+    type : string,
+    handler : (Event) => void,
+) : EventRegistrationToken;
+
+
+/**
+ * Registers an delegated event listener, that reacts to events thrown on specific child elements.
+ *
+ * Returns the intermediate function, so that the event listener can be removed:
+ *
+ *      const intermediate = delegate(element, selector, type, handler);
+ *      off(element, event, intermediate);
+ */
+declare function delegate (
+    element : HTMLElement | Window,
+    selector : string,
+    type : string,
+    handler : (Event, HTMLElement) => void,
+) : EventRegistrationToken;
+
+
+/**
+ * Dispatches an event
+ */
+declare function trigger (
+    element : HTMLElement | Window,
+    type : string,
+    data : any,
+) : void;

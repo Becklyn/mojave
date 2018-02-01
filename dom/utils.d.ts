308be5356f1be4d7eb48f947ae731d1b6ebdefa3
Jannik Zschiesche <jz@becklyn.com>
2018-02-01 17:58:13 +0100

    Add TS definitions for dom/utils

---
+++ dom/utils.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Checks whether the given node is an HTMLElement
+ */
+declare function isElement (
+    node : Node,
+) : boolean;
+
+
+/**
+ * Splits the string value by spaces
+ */
+declare function splitStringValue (
+    value : string | Array<string>,
+) : Array<string>;

100233aae12bfca1237269755fa2a00775fcec97
Jannik Zschiesche <jz@becklyn.com>
2018-03-25 20:07:31 +0200

    TS->JS: dom/attr

---
+++ dom/attr.js
@@ -0,0 +1,168 @@
+/* eslint-disable no-underscore-dangle */
+
+import {splitStringValue} from "./utils";
+
+const SPECIAL_ATTRIBUTE_SETTERS = /^(html|text|css)$/;
+
+
+/**
+ * Sets all attributes on the given element
+ *
+ * @param {Element} element
+ * @param {mojave.types.OptionalKeyMap} attributes
+ */
+export function setAttrs (element, attributes)
+{
+    for (const key in attributes)
+    {
+        if (!attributes.hasOwnProperty(key))
+        {
+            continue;
+        }
+
+        const value = attributes[key];
+
+        if (SPECIAL_ATTRIBUTE_SETTERS.test(key))
+        {
+            return;
+        }
+
+        if (value === null || value === false)
+        {
+            element.removeAttribute(key);
+        }
+        else
+        {
+            element.setAttribute(
+                key,
+                (value === true) ? key : ("" + value)
+            );
+        }
+    }
+}
+
+
+/**
+ * Returns the attribute value for the given html node
+ *
+ * @param {Element} element
+ * @param {string} attribute
+ * @returns {string | null}
+ */
+export function getAttr (element, attribute)
+{
+    return element.getAttribute(attribute);
+}
+
+
+/**
+ * Updates the classes on the given element
+ *
+ * @private
+ * @param {Element | Element[]} element
+ * @param {string | string[]} classes
+ * @param {string} method
+ */
+function updateClasses (element, classes, method)
+{
+    /** @type {Element[]} list */
+    const list = Array.isArray(element) ? element : [element];
+    const classList = splitStringValue(classes);
+
+    for (let i = 0; i < list.length; i++)
+    {
+        for (let j = 0; j < classList.length; j++)
+        {
+            list[i].classList[method](classList[j]);
+        }
+    }
+}
+
+
+/**
+ * Adds all given classes to the element
+ *
+ * @param {Element | Element[]} element
+ * @param {string | string[]} classes
+ */
+export function addClass (element, classes)
+{
+    updateClasses(element, classes, "add");
+}
+
+
+/**
+ * Remove all given classes from the element
+ *
+ * @param {Element | Element[]} element
+ * @param {string | string[]} classes
+ */
+export function removeClass (element, classes)
+{
+    updateClasses(element, classes, "remove");
+}
+
+
+/**
+ * Normalizes the key for *Data functions
+ *
+ * @private
+ * @param {string} key
+ * @returns {string}
+ */
+function normalizeDataKey (key)
+{
+    return key.replace(
+        /-([a-z])/g,
+        (matches) => matches[1].toUpperCase()
+    );
+}
+
+
+/**
+ * Sets the data on the given element
+ *
+ * @param {Element} element
+ * @param {string} key
+ * @param {*} value
+ */
+export function setData (element, key, value)
+{
+    key = normalizeDataKey(key);
+
+    if (element._data === undefined)
+    {
+        element._data = {};
+    }
+
+    element._data[key] = value;
+}
+
+
+/**
+ * Loads the data from the element
+ *
+ * @param {HTMLElement} element
+ * @param {string} key
+ * @returns {*}
+ */
+export function getData (element, key)
+{
+    const normalizedKey = normalizeDataKey(key);
+
+    if (typeof element._data === "object" && typeof element._data[normalizedKey] !== "undefined")
+    {
+        return element._data[normalizedKey];
+    }
+
+    // @legacy IE <= 10 doesn't support dataset
+    if (element.dataset === undefined)
+    {
+        return getAttr(element, `data-${key}`);
+    }
+
+    const value = element.dataset[normalizedKey];
+    return value === undefined
+        ? null
+        : value;
+}

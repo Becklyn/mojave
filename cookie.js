64cb7598e6e4242bc90177dbd900baff30520847
Kai Eichinger <ke@becklyn.com>
2018-05-17 15:40:04 +0200

    Fallback to an array when document.cookie is not available

--- cookie.js
+++ cookie.js
@@ -50,7 +50,7 @@ export function setCookie (key, value, options = {})
  */
 export function getCookie (key)
 {
-    const cookies = document.cookie.split("; ");
+    const cookies = document.cookie ? document.cookie.split('; ') : [];
 
     for (let i = 0; i < cookies.length; i++)
     {

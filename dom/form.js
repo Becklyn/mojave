1407fd9d2ba0c3c3494782a34378e1ebb0fe0e2e
Jannik Zschiesche <jz@becklyn.com>
2018-10-29 13:35:56 +0100

    Deprecate dom/form

--- dom/form.js
+++ dom/form.js
@@ -1,6 +1,7 @@
 /**
  * Serializes the form data
  *
+ * @deprecated Deprecated since 2.6, to be removed in 3.0. Inline the implementation instead.
  * @param {HTMLFormElement} form
  * @returns {FormData}
  */

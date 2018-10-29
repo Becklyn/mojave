8bbc2eb4c903b0ad329627eeb6ea49204ce1119e
Kai Eichinger <ke@becklyn.com>
2018-10-29 15:36:21 +0100

    Remove type

--- io/file.ts
+++ io/file.ts
@@ -24,7 +24,7 @@ export function formatSize (size : number) : string
     }
 
     const exponent = Math.min(Math.floor(Math.log(size) / Math.log(1000)), UNITS.length - 1);
-    let sizeFormatted : number|string = Number((size / Math.pow(1000, exponent)).toPrecision(3));
+    let sizeFormatted = Number((size / Math.pow(1000, exponent)).toPrecision(3));
 
     return `${prefix}${("" + sizeFormatted).replace(".", ",")} ${UNITS[exponent]}`;
 }

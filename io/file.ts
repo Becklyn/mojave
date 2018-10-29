0ea5b337855fa19b6e3baa13a24ab3f350ea9eeb
Kai Eichinger <cH40z-Lord@outlook.com>
2018-10-29 15:43:06 +0100

    Convert io/file to TS (#139)
    * Convert io/file to TS
    
    * Inline variables
    
    * Remove type

--- io/file.ts
+++ io/file.ts
@@ -7,11 +7,8 @@ export const UNITS = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
 /**
  * Formats the given file size in a human readable format
- *
- * @param {number} size
- * @returns {string}
  */
-export function formatSize (size)
+export function formatSize (size : number) : string
 {
     const isNegative = size < 0;
     const prefix = isNegative ? "-" : "";
@@ -28,8 +25,6 @@ export function formatSize (size)
 
     const exponent = Math.min(Math.floor(Math.log(size) / Math.log(1000)), UNITS.length - 1);
     let sizeFormatted = Number((size / Math.pow(1000, exponent)).toPrecision(3));
-    sizeFormatted = ("" + sizeFormatted).replace(".", ","); // eslint-disable-line prefer-template
-    const unit = UNITS[exponent];
 
-    return `${prefix}${sizeFormatted} ${unit}`;
+    return `${prefix}${("" + sizeFormatted).replace(".", ",")} ${UNITS[exponent]}`;
 }

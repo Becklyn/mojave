670b534e33949d1f42d076d27284b30e9892e560
Kai Eichinger <cH40z-Lord@outlook.com>
2018-10-29 16:18:31 +0100

    Convert dom/manipulate to TS (#136)
    * Convert dom/manipulate to TS
    
    * Inline types
    
    * Inline KeyMap

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,6 @@
 declare namespace mojave.types
 {
     /**
-     * Options for createElement()
-     */
-    type CreateElementOptions = KeyMap & {
-        css? : KeyMap,
-        text? : string,
-        html? : string,
-    }
-
-    /**
-     * Allowed argument for DOM insert methods.
-     */
-    type InsertableElement = string | Element | Element[];
-
-    /**
-     * Generic map from string keys -> string | number
-     */
-    type KeyMap = {[key : string] : string | number};
-
-    /**
      * Generic map from string keys -> string | number | null | boolean
      */
     type OptionalKeyMap = {[key : string] : string | number | null | boolean};

4fd9f1193527c42e0ab791319aab331e3c48c27c
Jannik Zschiesche <jz@becklyn.com>
2019-04-25 11:40:51 +0200

    Add docs, only resolve for 2xx & 400 and add success return type.

--- network.ts
+++ network.ts
@@ -10,16 +10,48 @@ export interface RequestOptions
     credentials?: boolean;
 }
 
-export type RequestFailureReasons = "status" | "invalid_json" | "request_failed";
-
+/**
+ * Promise return value if the request succeeded, and the promise resolves.
+ *
+ * The success flag indicates if the request was performed successfully (will be false if the status code was `400`).
+ */
 export interface SuccessResponse<T> {
     response: Response;
+    success: boolean;
     data: T;
 }
 
-export interface FailureResponse {
+
+/**
+ * All promise return values in the rejection cases.
+ */
+export type FailureResponse = RequestFailureResponse | JsonFailureResponse | StatusFailureResponse;
+
+/**
+ * Promise return value if the request failed
+ */
+export interface RequestFailureResponse {
     error: Error;
-    reason: RequestFailureReasons;
+    reason: "request_failed";
+}
+
+/**
+ * Promise return value if the JSON decoding failed
+ */
+export interface JsonFailureResponse {
+    error: Error;
+    response: Response;
+    reason: "invalid_json";
+}
+
+/**
+ * Promise return value if the HTTP status code indicated an (unexpected) error
+ */
+export interface StatusFailureResponse {
+    error: Error;
+    response: Response;
+    data: any;
+    reason: "invalid_json";
 }
 
 
@@ -40,7 +72,6 @@ export function request<T extends object = {}> (url: string, options: RequestOpt
         headers["Content-Type"] = "application/json; charset=UTF-8";
     }
 
-
     return new Promise(
         (resolve, reject) => {
             fetch(url, {
@@ -55,14 +86,16 @@ export function request<T extends object = {}> (url: string, options: RequestOpt
                             .then(
                                 data =>
                                 {
-                                    if (response.status < 200 || response.status >= 500)
+                                    // allow 400, as we pass validation errors as 400 and they should resolve the promise,
+                                    // as they are "expected" errors.
+                                    if ((response.status >= 200 && response.status < 300) || response.status === 400)
                                     {
-                                        return reject({response, data, reason: "status"});
+                                        return resolve({data, response, success: response.status !== 400});
                                     }
 
-                                    resolve({data, response});
+                                    reject({response, data, reason: "status"});
                                 },
-                                (error) => reject({error, reason: "invalid_json"})
+                                (error) => reject({response, error, reason: "invalid_json"})
                             );
                     },
                     (error) => reject({error, reason: "request_failed"}),

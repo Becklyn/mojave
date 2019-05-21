2f58e9e67078f5e424769068d9c4c16772eb715c
Jannik Zschiesche <jz@becklyn.com>
2019-05-21 10:34:04 +0200

    Change default value of `safeParseJson()`

--- json.ts
+++ json.ts
@@ -1,11 +1,11 @@
 /**
  * Wraps parsing of JSON, so that an error is logged, but no exception is thrown
  */
-function safeParseJson (value?: string|null) : {[k: string]: any}
+function safeParseJson (value?: string|boolean|null) : {[k: string]: any}|null
 {
     if (!value)
     {
-        return {};
+        return null;
     }
 
     try
@@ -13,12 +13,12 @@ function safeParseJson (value?: string|null) : {[k: string]: any}
         const content = value.trim();
         return (content !== "")
             ? JSON.parse(content)
-            : {};
+            : null;
     }
     catch (e)
     {
         console.error(`Could not parse JSON content: ${e.message}`, e);
-        return {};
+        return null;
     }
 }
 

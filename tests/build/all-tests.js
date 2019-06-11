d8ebd224b13485d58ffa0036b71a29cab265b660
Jannik Zschiesche <jz@becklyn.com>
2019-06-11 19:49:47 +0200

    Fix test file name

--- tests/build/all-tests.js
+++ tests/build/all-tests.js
@@ -34,7 +34,7 @@ import "../cases/dom/utils/splitStringValue";
 import "../cases/extend/extend";
 import "../cases/extend/merge";
 import "../cases/io/file";
-import "../cases/json/parseElementJson";
+import "../cases/json/parseElementAsJson";
 import "../cases/json/safeParseJson";
 import "../cases/mount/mount";
 import "../cases/runtime/hasOwnProperty";

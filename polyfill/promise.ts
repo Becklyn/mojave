e0e6771ea123c37d18f405bc17f8f11408cfb59c
Kai Eichinger <ke@becklyn.com>
2019-07-12 15:55:16 +0200

    Remove redundant global window assignment as the polyfill does it as well

--- polyfill/promise.ts
+++ polyfill/promise.ts
@@ -1,9 +1,1 @@
-//@ts-ignore
-import Promise from "promise-polyfill";
-
-let globalWindow = window as any;
-
-if (globalWindow.Promise === undefined)
-{
-    globalWindow.Promise = Promise;
-}
+import "promise-polyfill/src/polyfill";

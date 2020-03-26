f7787b526d99a3f4b38655991cf1d032bb7731e7
Jannik Zschiesche <jz@becklyn.com>
2020-03-26 17:16:48 +0100

    Add `useLiveRef()` hook

---
+++ hooks/live-ref.ts
@@ -0,0 +1,25 @@
+import {useCallback} from "preact/hooks";
+
+type LiveRefCleanup = () => void;
+type LiveRefCallback = (node: HTMLElement) => LiveRefCleanup|null|undefined|void;
+
+/**
+ * A "live ref" hook, that is always updated if the ref is updated.
+ */
+export function useLiveRef (callback: LiveRefCallback) : (node: HTMLElement|null) => void
+{
+    let cleanup: LiveRefCleanup|undefined|null;
+
+    return useCallback((node: HTMLElement|null) =>
+    {
+        if (cleanup)
+        {
+            cleanup();
+        }
+
+        if (node)
+        {
+            cleanup = callback(node) as LiveRefCleanup;
+        }
+    }, []);
+}

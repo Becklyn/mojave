291faf01e037b2686232e0fe9ca161f0f3e02d14
Jannik Zschiesche <jz@becklyn.com>
2020-07-24 16:38:58 +0200

    Add `DataContainer`

---
+++ data/DataContainer.ts
@@ -0,0 +1,33 @@
+import {parseElementAsJson} from "../json";
+
+
+/**
+ * Generic data container, that loads data from a JSON script element and
+ * provides a simple interface to load it.
+ */
+export class DataContainer
+{
+    private containerId: string;
+    private data: Record<string, any>;
+
+    public constructor (containerId: string)
+    {
+        this.containerId = containerId;
+    }
+
+    /**
+     * Fetch data from the data container
+     */
+    public get <TData>(key: string): TData|undefined;
+    public get <TData>(key: string, defaultValue: TData): TData;
+    public get <TData>(key: string, defaultValue?: TData): TData|undefined
+    {
+        if (!this.data)
+        {
+            console.log("load!");
+            this.data = parseElementAsJson(document.getElementById(this.containerId)) || {};
+        }
+
+        return this.data[key] || defaultValue;
+    }
+}

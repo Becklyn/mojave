cbb9dc4d30977dffc1260e66c1fbdb1d8b2c8da5
Jannik Zschiesche <jz@becklyn.com>
2020-07-24 21:27:29 +0200

    Remove debug output

--- data/DataContainer.ts
+++ data/DataContainer.ts
@@ -24,7 +24,6 @@ export class DataContainer
     {
         if (!this.data)
         {
-            console.log("load!");
             this.data = parseElementAsJson(document.getElementById(this.containerId)) || {};
         }
 
